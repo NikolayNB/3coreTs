2bf48507fe25c35733f2ec6eb8876fc6d2d65830
Nikolay Núñez <nikolaynunezballadares@gmail.com>
2023-08-02 16:47:04 +0200

    completed section on getters and setters

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,20 @@ class User {
     constructor(email, name) {
         this.email = email;
         this.name = name;
+        this._courseCount = 1;
         this.city = "Berlin";
+    }
+    get AppleEmail() {
+        return `apple${this.email}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error('Invalid course count');
+        }
+        this._courseCount = courseNum;
     }
 }
 ;
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,29 @@
 class User {
 
+    private _courseCount = 1;
+
     readonly city: string = "Berlin";
     constructor(
         public email: string, 
         public name: string,
         //private userId: string
         ){
+    }
+
+
+    get AppleEmail(): string{
+        return `apple${this.email}`
+    }
+
+    get courseCount(): number{
+        return this._courseCount;
+    }
+    
+    set courseCount(courseNum){
+        if(courseNum <= 1){
+            throw new Error('Invalid course count')
+        }
+        this._courseCount = courseNum;
     }
 };
 
