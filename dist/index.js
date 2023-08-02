3ac4f39ded9e8d14d8b19b90506574f3e77cf8ad
Nikolay Núñez <nikolaynunezballadares@gmail.com>
2023-08-02 16:26:42 +0200

    completed set up for projects

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,11 @@
 "use strict";
-console.log('Hello World! still');
+class User {
+    constructor(email, name) {
+        this.city = "Berlin";
+        this.email = email;
+        this.name = name;
+    }
+}
+;
+const niko = new User("niko@niko.com", "Niko");
+//niko.city = "Hamburg"; // Error: Cannot assign to 'city' because it is a read-only property.
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
-"use strict";
-console.log('Hello World! still');
+class User {
+
+    email: string;
+    name: string;
+    readonly city: string = "Berlin";
+
+    constructor(email: string, name: string){
+        this.email = email;
+        this.name = name;
+    }
+};
+
+const niko = new User("niko@niko.com", "Niko");
+//niko.city = "Hamburg"; // Error: Cannot assign to 'city' because it is a read-only property.
