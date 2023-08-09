577f622e9dc1b278e35645d699a760db1bac5fe6
Nikolay Núñez <nikolaynunezballadares@gmail.com>
2023-08-09 21:08:49 +0200

    Generic Classes

--- src/myGenerics.ts
+++ src/myGenerics.ts
@@ -38,3 +38,37 @@ const getMoreSearchProducts = <T>(products: T[]): T => {
     const myIndex = 4;
     return products[myIndex];
 };
+
+interface Database{
+    connection: string,
+    username: string,
+    password: string
+}
+
+function anotherFunction <T, U extends Database>(valOne: T, valTwo: U){
+    return{
+        valOne,
+        valTwo
+    };
+}
+
+anotherFunction(3, {connection: 'localhost', username: 'root', password: '1234'});
+
+interface Quiz{
+    name: string,
+    type: string
+}
+
+interface Course{
+    name: string,
+    author: string,
+    subject: string
+}
+
+class Sellable<T>{
+    public cart: T[] = [];
+
+    addToCart(products: T){
+        this.cart.push(products);
+    }
+}
