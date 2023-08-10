f326946e300b16ffd11fd2545b90f7b347a5c11a
Nikolay Núñez <nikolaynunezballadares@gmail.com>
2023-08-10 19:59:15 +0200

    narrowing operator "in"

--- src/detections.ts
+++ src/detections.ts
@@ -11,4 +11,22 @@ function provideId(id: string | null){
         return;
     }
     id.toLowerCase();
+};
+
+interface User {
+    name: string,
+    email: string
+}
+
+interface Admin {
+    name: string,
+    email: string,
+    isAdmin: boolean
+}
+
+function isAdminAccount(account: User | Admin){
+    if("isAdmin" in account){
+        return account.isAdmin;
+    }
+    return false;
 }
